50722f207b1b43c3cfb6e2e325683c427e7ec133
CHUN WEN LIN <yize00001@gmail.com>
2022-08-04 23:52:28 +0800

    FB_AutoLogin_1.0.2

--- FBlogin.py
+++ FBlogin.py
@@ -7,19 +7,19 @@ options = Options()
 options.add_argument("--disable-notifications")
 
 url = 'https://www.facebook.com/'
-driver = webdriver.Chrome("/chromedriver.exe") 
+driver = webdriver.Chrome("chromedriver.exe") # 輸入chromedriver.exe檔案位置 
 #輸入使用者帳號密碼
-useremail = '輸入email'
-userpassword = '輸入密碼'
+useremail = '輸入email'                     # 輸入帳號(信箱) 
+userpassword = '輸入密碼'                   # 輸入密碼 
 
 driver.get(url)
 # 執行登入
-enter_email = driver.find_element("id", "email")  # 輸入電子郵件
+enter_email = driver.find_element("id", "email")  # 自動輸入電子郵件
 enter_email.send_keys(useremail)
 
-enter_password = driver.find_element("id", "pass") # 輸入密碼
+enter_password = driver.find_element("id", "pass") # 自動輸入密碼
 enter_password.send_keys(userpassword)
 
-driver.find_element("name", "login").click() #按登入紐
+driver.find_element("name", "login").click() #按下登入紐
 time.sleep(5)
 
